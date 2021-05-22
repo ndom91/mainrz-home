3547e4dad801515e68f0b6d0af492b440550eebc
ndom91 <yo@ndo.dev>
2021-05-22 10:55:11 +0200

    fix: notino api route fetching

--- src/pages/api/notion.js
+++ src/pages/api/notion.js
@@ -43,8 +43,8 @@ export default async function handler(req, res) {
       }),
     })
     const data = await res.json()
-    return res.status(200).json({ data }).end()
+    return res.status(200).json({ data })
   } catch (err) {
-    return res.status(500).json({ err }).end()
+    return res.status(500).json({ err })
   }
 }
