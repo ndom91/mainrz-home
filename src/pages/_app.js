f8d6c8cd2207cf6dbe6f3ef0e4bd61a43738b3b8
ndom91 <yo@ndo.dev>
2021-06-10 20:02:08 +0200

    feat: add facebook pixel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import '../styles/index.css'
 
+function FacebookPixel() {
+  const router = useRouter()
+
+  React.useEffect(() => {
+    import('react-facebook-pixel')
+      .then(x => x.default)
+      .then(ReactPixel => {
+        ReactPixel.init(process.env.NEXT_PUBLIC_PIXEL_ID)
+        ReactPixel.pageView()
+
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView()
+        })
+      })
+  })
+  return null
+}
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <FacebookPixel />
+    </>
+  )
 }
 
 export default MyApp
