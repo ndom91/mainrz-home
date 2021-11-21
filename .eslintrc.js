78696a1901b7dec5840fc127f087f8a0558ea0a5
ndom91 <yo@ndo.dev>
2021-11-21 16:41:11 +0100

    fix: lint-staged eslint cmd

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parser: "next/babel",
+  plugins: [""],
+  rules: {
+    "react/prop-types": 0,
+    "react/jsx-uses-vars": [2],
+    "react/jsx-no-undef": "error",
+    "no-console": 0
+  }
+}
